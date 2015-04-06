afc902e9ad617bc1b65be8848c5cc0c0283702fa
Pablo Faust <pabfaust@gmail.com>
2015-04-06 16:30:26 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html class="no-js" lang="es_ES">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link type="text/css" rel="stylesheet" href="<?php echo $url ?>css/bootstrap.css" />
+  <link type="text/css" rel="stylesheet" href="<?php echo $url ?>css/style.css" />
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+  <title>Mi portafolio</title>
+</head>
+
+<body>
+
+<div class="container-fluid title">
+	<div class="row">
+		<div class="col-sm-3 column">
+			<a href="index.php"><button type="button" class="btn btn-lg"><h1>Portafolky</h1></button></a>
+			<h2>Aprende a programar un <strong>portafolio</strong> justo al perfil de tu <strong>creatividad :</strong></h2>
+		</div>
+		<div class="col-sm-7 col-sm-offset-1 column">
+			<div class="bubbles">
+				<div class="col-sm-3 sub-column">
+					<img src="img/circle1.png" alt="Structure Icon" />
+				</div>
+				<div class="col-sm-1 sub-column">
+					<p>+</p>
+				</div>
+				<div class="col-sm-3 sub-column">
+					<img src="img/circle2.png" alt="Edit Icon" />
+				</div>
+				<div class="col-sm-1 sub-column">
+					<p>=</p>
+				</div>
+				<div class="col-sm-3 sub-column">
+					<img src="img/circle3.png" alt="Portafolio Icon" />
+				</div>
+			</div>
+			<div class="demonstration">
+				<div class="col-sm-3 sub-column">
+					<h3>Te damos la estructura</h3>
+				</div>
+				<div class="col-sm-3 col-sm-offset-1 sub-column">
+					<h3>Bases de HTML & CSS</h3>
+				</div>
+				<div class="col-sm-3 col-sm-offset-1 sub-column">
+					<h3>Un portafolio personalizado</h3>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid presentation">
+	<div class="row">
+		<div class="col-sm-3 column">
+			<div class="subtitle">
+				<h4>Plan de estudio</h4>
+			</div>
+			<div class="agenda">
+				<h5><strong>1° sesión :</strong> lorem ipsum</h5>
+				<h5><strong>2° sesión :</strong> lorem ipsum</h5>
+				<h5><strong>3° sesión :</strong> lorem ipsum</h5>
+				<h5><strong>4° sesión :</strong> lorem ipsum</h5>
+				<h5><strong>5° sesión :</strong> lorem ipsum</h5>
+			</div>
+		</div>
+		<div class="col-sm-7 col-sm-offset-1 column">
+			<div class="structure">
+				<div class="col-sm-7 sub-column">
+					<img src="img/structure.png" alt="Portafolky's Portafolio Structure Devices" />
+				</div>
+				<div class="col-sm-5 sub-column">
+					<p class="text">Estructura</p>
+					<a href="#"><button type="button" class="btn"><p>Live Demo</p></button></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+</body>
+
+
+<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+<script src="<?php echo $url ?>js/bootstrap.min.js"></script>
+<script src="<?php echo $url ?>js/code.js"></script>
+
+</html>
